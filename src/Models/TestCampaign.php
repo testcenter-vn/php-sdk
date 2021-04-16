bc113e01e68e740ef97d2201dab69f12b078b7bc
Quyet Tran <quyettv.it@gmail.com>
2021-04-16 09:11:43 +0700

    update All testcampaign Model

--- src/Models/TestCampaign.php
+++ src/Models/TestCampaign.php
@@ -4,14 +4,34 @@ namespace Testcenter\Testcenter\Models;
 
 class TestCampaign
 {
-	public $id;
-	public $name;
+    public $id;
+    public $name;
+    public $category;
+    public $startAt;
+    public $endAt;
+    public $note;
+    public $editUrl;
 
     public function __construct($testCampaign)
     {
-    	$this->id = $testCampaign->id;
-    	if (isset($testCampaign->name)) {
-    		$this->name = $testCampaign->name;
-    	}
+        $this->id = $testCampaign->id;
+        if (isset($testCampaign->name)) {
+            $this->name = $testCampaign->name;
+        }
+        if (isset($testCampaign->category)) {
+            $this->category = $testCampaign->category;
+        }
+        if (isset($testCampaign->start_at)) {
+            $this->startAt = $testCampaign->start_at;
+        }
+        if (isset($testCampaign->end_at)) {
+            $this->endAt = $testCampaign->end_at;
+        }
+        if (isset($testCampaign->note)) {
+            $this->note = $testCampaign->note;
+        }
+        if (isset($testCampaign->edit_url)) {
+            $this->editUrl = $testCampaign->edit_url;
+        }
     }
 }
